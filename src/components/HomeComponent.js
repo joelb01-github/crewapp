ac8bb9aa91e9d19b9e039ca629f64779c5a13dd9
Joel Barenco <joelbarenco@gmail.com>
2019-01-12 09:26:56 +0000

    small improvement

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -9,7 +9,7 @@ const Home = () => (
       <p className="lead">Etais-tu trop bourr&eacute; ou sauras-tu t'en rappeler???</p>
       <hr className="my-2" />
       <h4>Regles du jeu</h4>
-      <p>30 secondes pour deviner un maximum de questions sur les photos qui te seront pr&eacute;sent&eacute;es.</p>
+      <p>10 questions sur les photos qui te seront pr&eacute;sent&eacute;es.</p>
       <p>1 bonne r&eacute;ponse = 1 point</p>
       <p>1 mauvaise r&eacute;ponse = - 0.5 point</p>
       <p className="lead">
