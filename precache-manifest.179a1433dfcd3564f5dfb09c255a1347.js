e93964d95198d999559b716b517e1e2dbb27f517
Joel Barenco <joelbarenco@gmail.com>
2019-01-12 09:37:45 +0000

    Updates

---
+++ precache-manifest.179a1433dfcd3564f5dfb09c255a1347.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/crewapp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "4910387e3485985f6c7d",
+    "url": "/crewapp/static/css/main.96a622b8.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/crewapp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "668ba351be0753ab48ef",
+    "url": "/crewapp/static/js/1.668ba351.chunk.js"
+  },
+  {
+    "revision": "70e1f85aab7994a64c1f",
+    "url": "/crewapp/static/js/runtime~main.70e1f85a.js"
+  },
+  {
+    "revision": "4910387e3485985f6c7d",
+    "url": "/crewapp/static/js/main.4910387e.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/crewapp/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/crewapp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/crewapp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "668ba351be0753ab48ef",
+    "url": "/crewapp/static/css/1.ab310c56.chunk.css"
+  },
+  {
+    "revision": "6f860f829d6a55609bd824144aa1a5d9",
+    "url": "/crewapp/index.html"
+  }
+];
